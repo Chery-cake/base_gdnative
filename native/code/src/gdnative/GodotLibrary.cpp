c6c1d60dcf3c6f8a7d70e6ca7eceb7b40efbe93e
Chery-cake <61635453+Chery-cake@users.noreply.github.com>
2022-04-04 19:15:37 -0300

    Update GodotLibrary.cpp

--- native/code/src/gdnative/GodotLibrary.cpp
+++ native/code/src/gdnative/GodotLibrary.cpp
@@ -2,21 +2,19 @@
 
 #include <AutoRegister.hpp>
 
-using namespace godot;
-
 /** GDNative Initialize **/
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
-	Godot::gdnative_init(o);
+	godot::Godot::gdnative_init(o);
 }
 
 /** GDNative Terminate **/
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
-	Godot::gdnative_terminate(o);
+	godot::Godot::gdnative_terminate(o);
 }
 
 /** NativeScript Initialize **/
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
-	Godot::nativescript_init(handle);
+	godot::Godot::nativescript_init(handle);
 
 	//utility classes
 
